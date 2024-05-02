c477cca251dcb9f7e154b6166d4d8ced017cc514
jainuttam185 <jainuttam185@gmail.com>
2024-05-02 16:34:30 +0530

    Made a post request to the moongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,29 +1,30 @@
-const fs = require('fs');
+// const fs = require('fs');
+const Tour=require('./../models/tourModel');
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-  );
+// const tours = JSON.parse(
+//     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+//   );
 
-exports.checkID = (req,res,next,val)=>{
-  console.log(`Tour id is: ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-    next();
-}; 
+// exports.checkID = (req,res,next,val)=>{
+//   console.log(`Tour id is: ${val}`);
+//   if (req.params.id * 1 > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID',
+//     });
+//   }
+//     next();
+// }; 
 
-exports.checkBody=(req,res,next)=>{
-  if(!req.body.name || !req.body.price){
-      return res.status(400).json({
-        status:'fail',
-        message:'Missing name or price'
-      }); 
-  }
-  next();
-}
+// exports.checkBody=(req,res,next)=>{
+//   if(!req.body.name || !req.body.price){
+//       return res.status(400).json({
+//         status:'fail',
+//         message:'Missing name or price'
+//       }); 
+//   }
+//   next();
+// }
 
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
@@ -51,23 +52,29 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
+exports.createTour = async (req, res) => {
+  // console.log(req.body);
+  // const newId = tours[tours.length - 1].id + 1;
+  // const newTour = Object.assign({ id: newId }, req.body);
+  // tours.push(newTour);
+  // fs.writeFile(
+  //   `${__dirname}/dev-data/data/tours-simple.json`,
+  //   JSON.stringify(tours),
+  //   (err) => {
+    try{
+    const newTour = await Tour.create(req.body);
       res.status(201).json({
         status: 'success',
         data: {
           tour: newTour,
         },
       });
+    } catch (err) {
+      res.status(400).json({
+        status:'failed',
+        message:'Invalid ID Sent'
+      })
     }
-  );
 };
 
 exports.updateTour = (req, res) => {
--- server.js
+++ server.js
@@ -11,29 +11,8 @@ mongoose.connect(DB,{
   console.log('DB connection successfull');
 });
 
-const tourSchema = new mongoose.Schema({
-       name:{
-        type:String,
-        required: [true,'A tour must have a name'],
-        unique:true
-       },
-       rating: {
-        type:Number,
-        default:4.5
-       },
-       price:{
-        type:Number,
-        required:[true,'A tour must have a price']
-       }
-});
-const Tour = mongoose.model('Tour',tourSchema);
 
-const testTour= new Tour({
-   name:'Sky Dive',
-   rating:4.9,
-   price:297
-});
-testTour.save().then(doc=>{console.log(doc)}).catch(err=>{console.log('ERROR')});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
