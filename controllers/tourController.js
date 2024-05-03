838b4775593ecb59f8934e35c8e439f395c1f940
jainuttam185 <jainuttam185@gmail.com>
2024-05-03 15:52:37 +0530

    Implemented Sorting Query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -28,17 +28,26 @@ const Tour=require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try {
-      //Build query
-      //1)Build Query
+    // Build query
+    // 1) Build Query
     const queryObj={...req.query};
     const excludeFields=['page','sort','limit','fields'];
     excludeFields.forEach(el=> delete queryObj[el]);
   
-    // 2)Advanced filtering  
+    // 2) Advanced filtering  
     let queryStr=JSON.stringify(queryObj);
     queryStr=queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match=>`$${match}`);
     console.log(JSON.parse(queryStr));
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 3) Sorting
+
+    if(req.query.sort){
+      const sortBy =req.query.sort.split(',').join(' ');
+      query=query.sort(sortBy);
+    }else{
+      query=query.sort('-createdAt');
+    }
     
     // const query = Tour.find()
     //     .where('duration')
