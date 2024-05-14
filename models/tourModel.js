2df9f6e3347566f6921d51bd0ef06e059f5c77bd
jainuttam185 <jainuttam185@gmail.com>
2024-05-14 15:45:36 +0530

    Implemented aggregate middelware and validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,13 +1,21 @@
 const mongoose=require('mongoose');
 const slugify=require('slugify');
+const validator=require('validator');
 
 const tourSchema = new mongoose.Schema({
     name:{
      type:String,
      required: [true,'A tour must have a name'],
-     unique:true
-    },
+     unique:true,
+     maxlength:[40,'A tour must have less or equal than 40 characters'],
+     minlength:[10,'A tour must have greater or equal than 10 characters'],
+    //  validate:[validator.isAlpha,'Tour name should only contain characters']
+    }, 
     slug:String,
+    secretTour:{
+        type:Boolean,
+        default:false
+    },
     duration:{
         type:Number,
         required:[true,'A tour must have duration']
@@ -18,11 +26,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty:{
         type:String,
-        required:[true,'A tour must have difficulty']
+        required:[true,'A tour must have difficulty'],
+        enum:{
+            values: ['easy','medium','difficult'],
+            message:'Difficulty is either :easy,medium,difficult'
+        }
     },
     ratingAverage:{
         type:Number,
-        default:4.5
+        default:4.5,
+        min:[1,'Rating must be above 1.0'],
+        max:[5,'Rating must be below 5.0']
     },
     ratingQuantity: {
      type:Number,
@@ -32,7 +46,15 @@ const tourSchema = new mongoose.Schema({
      type:Number,
      required:[true,'A tour must have a price']
     },
-    priceDiscount:Number,
+    priceDiscount:{
+        type:Number,
+        validate:{
+            validator:function(val){
+                return val<this.price;
+              },
+              message:'Discount price should be below regular price'
+        }
+    },
     summary:{
         type:String,
         trim:true,
@@ -68,7 +90,7 @@ tourSchema.pre('save',function(next){
     this.slug=slugify(this.name,{lower:true});
     next();
 });
-
+//Query Middelware
 tourSchema.pre(/^find/,function(next){
     this.find({secretTour:{$ne:true}});
     this.start=Date.now();
@@ -76,9 +98,16 @@ tourSchema.pre(/^find/,function(next){
 });
 tourSchema.post(/^find/,function(docs,next){
      console.log(`Query took ${Date.now()-this.start} millisecounds`);
-     console.log(docs);
+     //console.log(docs);
      next();
 });
+//Aggregation Middelware
+tourSchema.pre('aggregate',function(next){
+      this.pipeline().unshift({$match:{secretTour:{$ne:true}}});
+      //console.log(this.pipeline());
+      next();
+});
+
 const Tour = mongoose.model('Tour',tourSchema);
 
 module.exports=Tour;
