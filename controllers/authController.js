7eb5fc1c2864a703a74e47017562ef0ec41205a7
jainuttam185 <jainuttam185@gmail.com>
2024-05-28 16:55:51 +0530

    changed he error from return next to res in login

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,7 +45,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
   //1) Check if email and password exist
   if (!email || !password) {
-    return next(new AppError('Please provide email and password', 400));
+    res.status(400).json({error: new AppError('Please provide email and password', 400)});
   }
   //2) Check if user exists && password is correct
   const user = await User.findOne({ email }).select('+password');
